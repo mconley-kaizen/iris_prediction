66cc015cb9e10a1b54e41385950fa53af1dfff42
Matthew Conley <matthewt.conley+kaizen@gmail.com>
2016-09-14 21:30:46 -0700

    remove expected name

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
-setup(name='iris_prediction',
-      version='0.1',
+setup( version='0.1',
       description='Identify iris from sepal and petal information',
       url='https://github.com/mconley-kaizen/iris_prediction.git',
       packages=['iris_prediction'],
