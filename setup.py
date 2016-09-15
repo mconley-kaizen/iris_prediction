24a6e5eaec3f2bccb43b069ac0e84a489e7271c6
Matthew Conley <matthewt.conley+kaizen@gmail.com>
2016-09-14 21:32:01 -0700

    replace name in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
-setup( version='0.1',
+setup(name='iris_prediction',
+      version='0.1',
       description='Identify iris from sepal and petal information',
       url='https://github.com/mconley-kaizen/iris_prediction.git',
       packages=['iris_prediction'],
