15a0efd3733ba966fa2c84980cd78e7657f474a5
Matthew Conley <mconley@teslamotors.com>
2016-09-14 19:02:17 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='iris_prediction',
+      version='0.1',
+      description='Identify iris from sepal and petal information',
+      url='http://github.com/mtconley/iris_prediction.git'
+      packages=['iris_prediction'],
+      zip_safe=False)
