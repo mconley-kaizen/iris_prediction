13edcfe6d2848c175a3265e4c877e25e93bef1da
Matthew Conley <matthewt.conley+kaizen@gmail.com>
2016-09-14 21:23:16 -0700

    missing comma in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='iris_prediction',
       version='0.1',
       description='Identify iris from sepal and petal information',
-      url='http://github.com/mtconley/iris_prediction.git'
+      url='https://github.com/mconley-kaizen/iris_prediction.git',
       packages=['iris_prediction'],
       zip_safe=False)
