bc866bae69d952bbaeb87ca48dcc06550a240ea5
Matthew Conley <matthewt.conley+kaizen@gmail.com>
2016-09-15 00:15:04 -0700

    include non-code data

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='iris_prediction',
+setup(
+      name='iris_prediction',
       version='0.1',
       description='Identify iris from sepal and petal information',
       url='https://github.com/mconley-kaizen/iris_prediction.git',
-      packages=['iris_prediction'],
-      zip_safe=False)
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={'': ['model.pkl']}
+)
